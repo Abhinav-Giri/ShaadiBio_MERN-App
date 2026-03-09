0dcf92aa3b2921abe0b33703540b55ab5b60bd08
Abhinav-Giri <iamabhinavgiri@gmail.com>
2026-03-09 20:36:01 +0530

    fixed unreachable imports

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+import errorHandler from "./middlewares/errorMiddleware.js";
+import notFound from "./middlewares/notFound.js";
 import authRoutes from "./routes/authRoutes.js";
 import biodataRoutes from "./routes/biodataRoutes.js";
 
