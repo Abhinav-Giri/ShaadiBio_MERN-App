54f1c1d083e0ae688a81f5475dfb399dc14130d4
Abhinav-Giri <iamabhinavgiri@gmail.com>
2026-03-10 00:05:02 +0530

    fixed current url in cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://shaadi-bio-rose.vercel.app"
+  "https://shaadibio-one.vercel.app/"
 ];
 
 app.use(cors({
